588ab6a9f1a28e5bf93c70b43947fce4c22b3ba6
Richard Brunt <r.brunt@gmail.com>
2013-06-22 21:04:10 +0100

    added page build time to footer

--- application/views/_footer.php
+++ application/views/_footer.php
@@ -4,6 +4,7 @@
 		<div class="container">
 			<p>Designed an built by <a href="http://richardbrunt.co.uk">Richard Brunt</a> for <a href="http://diccu.co.uk">DICCU</a></p>
 			<p>This is an open source project - get the source code at <a href="https://github.com/rbrunt/talks-manager">Github</a></p>
+			<p><small>Page took {elapsed_time} seconds to build</small></p>
 		</div>
 	</footer>
 

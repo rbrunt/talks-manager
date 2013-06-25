eb2e2db54d3a60af54482f50a8471bf66a880132
Richard Brunt <r.brunt@gmail.com>
2013-06-25 10:57:46 +0100

    removed files properly after moving

--- application/views/login_page.php
+++
@@ -1,6 +0,0 @@
-	<form class="form-signin">
-		<h2 class="form-signin-heading">Please sign in</h2>
-        <input type="text" class="input-block-level" placeholder="Email address">
-        <input type="password" class="input-block-level" placeholder="Password">
-        <button class="btn btn-large btn-primary" type="submit">Sign in</button>
-      </form>

95feb6203466fa6a2944a87afc97e94d84155b67
Richard Brunt <r.brunt@gmail.com>
2013-07-14 11:29:52 +0100

    Made admin homepage better

--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -2,7 +2,7 @@
 	<div class="span12">
 	<div class="page-header">
 		<h1>Admin Page <small>That's Administration to you!</small></h1>
-
+		<a class="btn btn-danger pull-right" href="<?php echo base_url("/admin/logout/"); ?>">Logout</a>
 		<p>Select an option below:</p>
 	</div>
 </div>
@@ -12,8 +12,9 @@
 		<p><a href="<?php echo base_url('/admin/talks/'); ?>">Edit Talks</a><p>
 		<p><a href="<?php echo base_url('/admin/series/'); ?>">Edit Series</a><p>
 		<p><a href="<?php echo base_url('/admin/speakers/'); ?>">Edit Speakers</a><p>
-		<p><a href="<?php echo base_url('/admin/addtalk/'); ?>">Add Talk</a><p>
-		<p><a href="<?php echo base_url('/admin/addseries/'); ?>">Add Series</a><p>
-		<p><a href="<?php echo base_url('/admin/addspeaker/'); ?>">Add Speaker</a><p>
+		<p><a class="muted" href="<?php echo base_url('/admin/addtalk/'); ?>">Add Talk</a><p>
+		<p><a class="muted" href="<?php echo base_url('/admin/addseries/'); ?>">Add Series</a><p>
+		<p><a class="muted" href="<?php echo base_url('/admin/addspeaker/'); ?>">Add Speaker</a><p>
+		<p><a class="text-success" href="<?php echo base_url('/admin/adduser/'); ?>">Add User</a><p>
 	</div>
 </div>

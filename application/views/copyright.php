d3d7758e1f8ab213856e22907576c440e6d201ca
Richard Brunt <r.brunt@gmail.com>
2013-10-19 12:17:04 +0100

    Changed copyright policy

--- application/views/copyright.php
+++ application/views/copyright.php
@@ -1,25 +1,7 @@
-	<h1>Copyright <small>mmm, yummy!</small></h1>
-	<p>This websie uses cookies to make your experience better. Cookies are little text files that sit on your computer. Your browser sends them to the website that set them every time you browse a page on it. They let the website provide enhanced features like the ability to log in, or keeping items in a shopping cart. Cookies are automatically deleted after a time set by the page that set them.</p>
-	<p>We set 2 cookies when you visit the site:</p>
+	<h1>Copyright Policy</h1>
+	<h3>Content</h3>
+	<p>All content on this website (talks, series artwork and descriptions) are Copyright &copy; 2013 DICCU. All rights reserved. </p>
+	<p>Feel free to download talks, and share them with others, just don't take money for them.</p>
 
-	<table class="table table-hover">
-		<thead>
-			<tr>
-				<th>Cookie</th>
-				<th>Description</th>
-				<th>Expiry</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td><code>ci_session</code></td>
-				<td>Session cookie. It is used to do things like display messages based on your actions, and is used when you log in. This cookie is set when you visit any page of the site, whether or not you log in.</td>
-				<td>When you close your browser window</td>
-			</tr>
-			<tr>
-				<td><code>new_visitor</code></td>
-				<td>This is set when you first visit the site. It means you don't see the message about cookies on every page.</td>
-				<td>After 30 days</td>
-			</tr>
-		</tbody>
-	</table>
+	<h3>This website</h3>
+	<p>This website is an open source project, and so the source code to run it is freely available. If you'd like to look at or download the source code it's available on <a target="_blank" href="https://github.com/rbrunt/talks-manager/">Gitub</a></p>

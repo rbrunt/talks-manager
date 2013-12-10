73cc2c104754f6955a9e6a7d43c6287aa7381e92
Richard Brunt <r.brunt@gmail.com>
2013-12-10 16:18:54 +0000

    Fixed typo in copyright page

--- application/views/copyright.php
+++ application/views/copyright.php
@@ -4,4 +4,4 @@
 	<p>Feel free to download talks, and share them with others, just don't take money for them.</p>
 
 	<h3>This website</h3>
-	<p>This website is an open source project, and so the source code to run it is freely available. If you'd like to look at or download the source code it's available on <a target="_blank" href="https://github.com/rbrunt/talks-manager/">Gitub</a></p>
+	<p>This website is an open source project, and so the source code to run it is freely available. If you'd like to look at or download the source code it's available on <a target="_blank" href="https://github.com/rbrunt/talks-manager/">Github</a></p>

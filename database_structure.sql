00b1a6b2554311bb91e0342693de3e1737a2af03
Richard Brunt <r.brunt@gmail.com>
2013-12-10 16:31:50 +0000

    Update DB structure

--- database_structure.sql
+++ database_structure.sql
@@ -1,30 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 3.4.5
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Dec 09, 2013 at 12:49 AM
--- Server version: 5.5.16
--- PHP Version: 5.3.8
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Database: `talks-manager`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ci_sessions`
---
 
 CREATE TABLE IF NOT EXISTS `ci_sessions` (
   `session_id` varchar(40) NOT NULL DEFAULT '0',
@@ -36,12 +17,6 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
   KEY `last_activity_idx` (`last_activity`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `series`
---
-
 CREATE TABLE IF NOT EXISTS `series` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL,
@@ -49,12 +24,6 @@ CREATE TABLE IF NOT EXISTS `series` (
   PRIMARY KEY (`id`),
   KEY `title` (`title`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `talks`
---
 
 CREATE TABLE IF NOT EXISTS `talks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -71,29 +40,18 @@ CREATE TABLE IF NOT EXISTS `talks` (
   KEY `SeriesIdIndex` (`seriesid`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(100) NOT NULL,
   `name` varchar(64) NOT NULL,
   `password` varchar(128) NOT NULL,
   `salt` varchar(64) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '0',
   `token` varchar(32) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `talks`
---
 ALTER TABLE `talks`
   ADD CONSTRAINT `talks_ibfk_1` FOREIGN KEY (`seriesid`) REFERENCES `series` (`id`) ON UPDATE CASCADE;
 

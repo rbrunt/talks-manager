67729e24614d12718697dca305091fb5858e9c7a
Richard Brunt <r.brunt@gmail.com>
2014-05-10 17:34:46 +0100

    Add Opengraph meta tags to talks details page for facebook embedding

--- application/views/includes/_header.php
+++ application/views/includes/_header.php
@@ -8,14 +8,33 @@
 	<title>DICCU Talks</title>
 <?php endif; ?>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="">
-	<meta name="author" content="DICCU">
+	<link rel="shortcut icon" href="<?php echo base_url('/favicon.ico')?>">
+	<meta name="description" content="<?php echo isset($description) ? $description : ""?>">
+	<meta name="author" content="Durham Inter-Collegiate Christian Union">
 
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/bootstrap/css/bootstrap.min.css')?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/bootstrap/css/style.css')?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/bootstrap/css/datepicker.css')?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('/static/font-awesome/css/font-awesome.min.css'); ?>">
 	<!-- <link href="/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet"> -->
+
+<?php if(isset($is_talk_page)):?>
+	<!-- Begin opengraph meta tags -->
+		<!-- Facebook -->
+			<meta property="og:title" content="<?php echo $talk[0]->title ;?>">
+			<meta property="og:type" content="music.song">
+			<meta property="og:image" content="<?php echo $artwork ;?>">
+			<meta property="og:url" content="<?php echo base_url("/talks/talk/".$talk[0]->id);?>">
+			<meta property="og:site_name" content="DICCU Talks">
+			<meta property="og:description" content="<?php echo $description;?>">
+			<meta property="music:album" content="<?php echo $talk[0]->seriestitle;?>">
+			<meta property="music:album:url" content="<?php echo base_url("/series/seriesdetail/".$talk[0]->seriesid);?>">
+			<meta property="music:release_date" content="<?php echo $talk[0]->date;?>">
+			<?php if ($talk_exists):?><meta property="og:audio:url" content="<?php echo base_url("/files/talks/".$talk[0]->id.".mp3");?>"><meta property="og:audo:type" content="audio/mpeg"><?php endif;?>
+
+	<!-- End Opengraph meta tags -->
+<?php endif; ?>
+
 	<script>
 		var base_url = "<?php echo base_url(); ?>";
 	</script>

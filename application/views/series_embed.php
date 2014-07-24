f8ab38da4d3aee35c19365ea1cfdefcad8e04dc8
Richard Brunt <r.brunt@gmail.com>
2014-07-24 20:55:29 +0100

    Add ability to embed a series detail page for use on the CU website

---
+++ application/views/series_embed.php
@@ -0,0 +1,41 @@
+	<div class="row">
+		<?php if($options["hide_artwork"]==false):?><div style="float:left;width:250px;max-width:25%;">
+			<img src="<?php echo $artwork; ?>">
+		</div><?php endif; ?>
+			<?php if($options["hide_title"]==false):?><div class="page-header">
+				<h3><?php echo $series[0]->title ?></h3>
+			</div><?php endif; ?>
+			<?php if($options["hide_description"]==false):?><div id="summary"><?php echo $this->typography->auto_typography($series[0]->summary) ?></div><?php endif; ?>
+		<div class="span12" style="overflow:hidden;">
+			<div id="talkstable">
+				<?php if($talks) : ?>
+				<table class="table table-hover">
+					<thead>
+						<th>Title</th>
+						<th>Date</th>
+						<th>Speaker</th>
+					</thead>
+					<tbody>
+						<?php foreach($talks as $talk): ?>
+						<tr>
+							<td><a href="<?php echo base_url('/talks/talk/'.$talk->id);?>"><?php echo $talk->title;?></a></td>
+							<td><?php echo $talk->date;?></td>
+							<?php if($talk->speakername==""): ?>
+								<td>-</td>
+							<?php else: ?>
+								<td><?php echo $talk->speakername; ?></td>
+							<?php endif; ?>
+						</tr>
+					<?php endforeach;?>
+					</tbody>
+				</table>
+				<?php else : ?>
+					<?php if($isLoggedIn): ?>
+						<p><strong>No talks yet!</strong> Click <a href="<?php echo base_url('admin/addtalk'); ?>">here</a> to add a talk</p>
+					<?php else: ?>
+						<p><strong>No talks yet!</strong> Someone will add some soon!</p>
+					<?php endif;?>
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>

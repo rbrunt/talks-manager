42effe9975b5a4fa51dd7a1a4b61342dfd24fab5
Richard Brunt <r.brunt@gmail.com>
2014-08-14 00:32:43 +0100

    Change Password page

--- application/views/admin/change_password.php
+++ application/views/admin/change_password.php
@@ -1,13 +1,32 @@
 <div class="row">
-  <div class="span12">
-
-  <?php echo form_open(); ?>
-  		<p>Enter your current password, followed by your new one (twice!)</p>
-        <?php echo form_password(array("name"=>"password", "class"=>"span12", "placeholder"=>"Current Password", "required"=>"True")); ?>
-        <?php echo form_password(array("name"=>"password1", "class"=>"span12", "placeholder"=>"Choose a Password", "required"=>"True")); ?>
-       	<?php echo form_password(array("name"=>"password2", "class"=>"span12", "placeholder"=>"Password Again", "required"=>"True")); ?>
-        <?php echo form_submit(array("class"=>"btn btn-primary", "value"=>"Change Password")); ?>
-      	<?php echo form_close(); ?>
+  <div class="col-md-8 col-md-offset-2">
+  		<h1>Change Password</h1>
+  		<p>Enter your current password followed by your new one (twice!) to change it.</p>
+  <?= form_open("", array("role"=>"form", "class"=>"form-horizontal")); ?>
+        <div class="form-group">
+        	<?= form_label("Password", "inputPassword", array("class"=>"col-sm-3 control-label"));?>
+        	<div class="col-sm-9">
+        		<?= form_password(array("name"=>"password", "class"=>"form-control", "placeholder"=>"Current Password", "required"=>"True", "id"=>"inputPassword")); ?>
+        	</div>
+        </div>
+        <div class="form-group">
+        	<?= form_label("New Password", "inputNewPassword1", array("class"=>"col-sm-3 control-label"));?>
+        	<div class="col-sm-9">
+        		<?= form_password(array("name"=>"password1", "class"=>"form-control", "placeholder"=>"Choose a Password", "required"=>"True", "id"=>"inputNewPassword1")); ?>
+        	</div>
+        </div>
+        <div class="form-group">
+        	<?= form_label("New Password Again", "inputNewPassword2", array("class"=>"col-sm-3 control-label"));?>
+       		<div class="col-sm-9">
+       			<?= form_password(array("name"=>"password2", "class"=>"form-control", "placeholder"=>"Password Again", "required"=>"True", "id"=>"inputNewPassword2")); ?>
+       		</div>
+       	</div>
+        <div class="form-group">
+        	<div class="col-sm-9 col-sm-offset-3">
+        		<?= form_submit(array("class"=>"btn btn-primary", "value"=>"Change Password")); ?>
+        	</div>
+        </div>
+      	<?= form_close(); ?>
 
 </div>
 </div>

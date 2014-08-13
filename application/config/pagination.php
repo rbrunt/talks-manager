0c9ebbab70da0d9c1a065b87f4dd263568858a7e
Richard Brunt <r.brunt@gmail.com>
2014-08-14 00:08:25 +0100

    Pagination

--- application/config/pagination.php
+++ application/config/pagination.php
@@ -27,29 +27,29 @@ $config['page_query_string'] = FALSE;
 // $config['use_page_numbers'] = TRUE;
 $config['query_string_segment'] = 'page';
 
-$config['full_tag_open'] = '<div class="pagination"><ul>';
-$config['full_tag_close'] = '</ul></div><!--pagination-->';
+$config['full_tag_open'] = '<div class="row"><div class="col-md-12"><ul class="pagination">';
+$config['full_tag_close'] = '</ul></div></div><!--pagination-->';
 
-$config['first_link'] = '&laquo; First';
-$config['first_tag_open'] = '<li class="prev page">';
+$config['first_link'] = '<i class="fa fa-angle-double-left"></i>';
+$config['first_tag_open'] = '<li>';
 $config['first_tag_close'] = '</li>';
 
-$config['last_link'] = 'Last &raquo;';
-$config['last_tag_open'] = '<li class="next page">';
+$config['last_link'] = '<i class="fa fa-angle-double-right"></i>';
+$config['last_tag_open'] = '<li>';
 $config['last_tag_close'] = '</li>';
 
-$config['next_link'] = 'Next &rarr;';
-$config['next_tag_open'] = '<li class="next page">';
+$config['next_link'] = '<i class="fa fa-angle-right"></i>';
+$config['next_tag_open'] = '<li>';
 $config['next_tag_close'] = '</li>';
 
-$config['prev_link'] = '&larr; Previous';
-$config['prev_tag_open'] = '<li class="prev page">';
+$config['prev_link'] = '<i class="fa fa-angle-left"></i>';
+$config['prev_tag_open'] = '<li>';
 $config['prev_tag_close'] = '</li>';
 
 $config['cur_tag_open'] = '<li class="active"><a href="">';
 $config['cur_tag_close'] = '</a></li>';
 
-$config['num_tag_open'] = '<li class="page">';
+$config['num_tag_open'] = '<li>';
 $config['num_tag_close'] = '</li>';
 
 // $config['display_pages'] = FALSE;

9cc49f256406a39b9f5e633d693cd8dc0d8728dd
Richard Brunt <r.brunt@gmail.com>
2014-08-13 23:07:50 +0100

    Admin view pages

--- application/views/admin/all_series_table.php
+++ application/views/admin/all_series_table.php
@@ -1,20 +1,20 @@
 		<div class="row">
-			<div class="span12">
+			<div class="col-md-12">
 				<ul class="breadcrumb">
-					<!-- <li><a href="/">Home</a> <span class="divider">/</span></li> -->
-					<li><a href="<?php echo base_url('/admin/')?>">Admin</a> <span class="divider">/</span></li>
+					<li><a href="<?php echo base_url('/admin/')?>">Admin</a></li>
 					<li class="active">Series</li>
 				</ul>
 			</div>
 		</div>
 		<div class="row">
-			<div class="span12">
+			<div class="col-md-12">
+			<div class="table-responsive">
 		<table class="table table-hover table-condensed">
 				<thead>
 					<th>Title</th>
 					<th>Summary</th>
 					<th>#&nbspTalks</th>
-					<th><a href="<?php echo base_url("admin/addseries"); ?>" class="btn btn-mini" title="Add a Series"><i class="icon-plus"></i></a></th>
+					<th><a href="<?php echo base_url("admin/addseries"); ?>" class="btn btn-sm btn-default btn-block" title="Add a Series"><i class="fa fa-plus"></i></a></th>
 				</thead>
 				<tbody>
 <?php foreach($series as $single_series): ?>
@@ -22,11 +22,12 @@
 						<td><strong><a href="<?php echo base_url('/series/seriesdetail/'.$single_series->id)?>"><?php echo $single_series->title;?></a></strong></td>
 						<td><?php echo character_limiter($single_series->summary, 100) ;?></td>
 						<td><?php echo $single_series->numtalks; ?></td>
-						<td><a href="<?php echo base_url('/admin/editseries/'.$single_series->id)?>" title="Edit" class="btn btn-mini btn-primary">Edit</a></td>
+						<td><a href="<?php echo base_url('/admin/editseries/'.$single_series->id)?>" title="Edit" class="btn btn-sm btn-primary">Edit</a></td>
 					</tr>
 <?php endforeach;?>
 				</tbody>
 			</table>
+			</div>
 	</div>
 <?php echo $this->pagination->create_links(); ?>
 </div>
--- application/views/admin/users.php
+++ application/views/admin/users.php
@@ -1,30 +1,31 @@
 	<div class="row">
-		<div class="span12">
+		<div class="col-md-12">
 			<ul class="breadcrumb">
-				<!-- <li><a href="/">Home</a> <span class="divider">/</span></li> -->
-				<li><a href="<?php echo base_url('/admin/')?>">Admin</a> <span class="divider">/</span></li>
+				<li><a href="<?php echo base_url('/admin/')?>">Admin</a></li>
 				<li class="active">Talks</li>
 			</ul>
 		</div>
 	</div>
 	<div class="row">
-		<div class="span12">
+		<div class="col-md-12">
+		<div class="table-responsive">
 			<table class="table table-hover">
 				<thead>
 					<th>Name</th>
 					<th>Email</th>
-					<th><a href="<?php echo base_url("admin/adduser"); ?>" class="btn btn-mini pull-right" title="Add a User"><i class="icon-plus"></i></a></th>
+					<th><a href="<?php echo base_url("admin/adduser"); ?>" class="btn btn-sm btn-default pull-right" title="Add a User"><i class="fa fa-plus"></i> Add User</a></th>
 				</thead>
 				<tbody>
 <?php foreach($users as $user): ?>
 					<tr class="activate-tooltip <?php echo ($user->active ? "" : "text-warning"); ?>" title="<?php echo ($user->active ? "" : "This user hasn't been activated - they'll need to set a password using the link they got before they can log in."); ?>" data-placement="bottom">
 						<td><?php echo (($user->name == "") ? "-" : $user->name ); ?></td>
 						<td><?php echo $user->email; ?></td>
-						<td><a href="<?php echo base_url('/admin/deleteuser/'.$user->id) ;?>" class="text-error pull-right"><i class="icon-remove"></i> Delete User</a></td>
+						<td><a href="<?php echo base_url('/admin/deleteuser/'.$user->id) ;?>" class="text-danger pull-right"><i class="fa fa-trash-o"></i> Delete User</a></td>
 					</tr>
 <?php endforeach;?>
 				</tbody>
 			</table>
+			</div>
 <?php echo $this->pagination->create_links(); ?>
 		</div>
 	</div>

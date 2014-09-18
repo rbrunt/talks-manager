1d5ab239933274b0b8d69e8e34f7c807010b483b
Richard Brunt <r.brunt@gmail.com>
2014-09-18 17:06:02 +0100

    Remove more unused views for speaker editing and browsing

--- application/views/admin/edit_speaker.php
+++
@@ -1,9 +0,0 @@
-	<div class="row">
-		<div class="span12">
-			<?php echo form_open("admin/editspeaker"); ?>
-			<?php echo form_input(array("name"=>"name", "value"=>$speaker[0]->name, "class"=>"h1 span12", "maxlength"=>"100", "placeholder"=>"Speaker Name")); ?></h1>
-			<?php echo form_submit(array("value"=>"Submit Edits", "class"=>"btn btn-primary")); ?>
-			<?php echo form_hidden(array("id"=>$speaker[0]->id)); ?>
-			<?php echo form_close(); ?>
-		</div>
-	</div>

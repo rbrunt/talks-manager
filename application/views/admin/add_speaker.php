9daa10db02c8d58c157fe49ecf21c5baf8a8ae64
Richard Brunt <r.brunt@gmail.com>
2014-09-18 17:05:20 +0100

    Remove redunant add speaker view

--- application/views/admin/add_speaker.php
+++
@@ -1,8 +0,0 @@
-	<div class="row">
-		<div class="span12">
-			<?php echo form_open("admin/addspeaker"); ?>
-			<?php echo form_input(array("name"=>"name", "class"=>"h1 span12", "maxlength"=>"100", "placeholder"=>"Speaker Name")); ?></h1>
-			<?php echo form_submit(array("value"=>"Add Speaker", "class"=>"btn btn-primary")); ?>
-			<?php echo form_close(); ?>
-		</div>
-	</div>

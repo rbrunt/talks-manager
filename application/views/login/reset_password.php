35614ba651d8d5d8ca0fad8c88bcd87057bec266
Richard Brunt <r.brunt@gmail.com>
2014-09-20 17:55:34 +0100

    Update signup views to bootstrap 3

--- application/views/login/reset_password.php
+++ application/views/login/reset_password.php
@@ -1,10 +1,23 @@
 <div class="row">
-  <div class="span12">
-
-  <?php echo form_open(); ?>
+	<div class="col-sm-6 col-sm-offset-3">
+		<h1>Reset Password</h1>
   		<p>Please enter your email address below, and we'll email you a password reset link.</p>
-        <?php echo form_input(array("name"=>"email","type"=>"email", "class"=>"span12", "placeholder"=>"Email Address", "value"=>set_value("email", ""), "required"=>"True")); ?>
-        <?php echo form_submit(array("class"=>"btn btn-primary", "value"=>"Reset Password")); ?>
+		
+		<?= form_open("",array("class"=>"form-horizontal")); ?>
+        
+	        <div class="form-group">
+	        	<?= form_label("Email", "email", array("class"=>"col-sm-2 control-label"));?>
+	        	<div class="col-sm-10">
+		        	<?= form_input(array("name"=>"email", "id"=>"email","type"=>"email", "class"=>"form-control", "placeholder"=>"Email Address", "value"=>set_value("email", ""), "required"=>"True")); ?>
+	        	</div>
+	        </div>
+	    
+	        <div class="form-group">
+	        	<div class="col-sm-10 col-sm-offset-2">
+	        		<?= form_submit(array("class"=>"btn btn-primary", "value"=>"Reset Password")); ?>
+	        	</div>
+	        </div>
+
       	<?php echo form_close(); ?>
-</div>
+	</div>
 </div>

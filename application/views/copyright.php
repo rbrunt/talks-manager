a3c70b489f1c906035b234e89ca9b7f4204989d2
Richard Brunt <r.brunt@gmail.com>
2015-01-30 22:59:39 +0000

    Update copyright year

--- application/views/copyright.php
+++ application/views/copyright.php
@@ -2,7 +2,7 @@
 	<div class="col-md-12">
 		<h1>Copyright Policy</h1>
 		<h3>Content</h3>
-		<p>All content on this website (talks, series artwork and descriptions) are Copyright &copy; 2013 DICCU. All rights reserved. </p>
+		<p>All content on this website (talks, series artwork and descriptions) are Copyright &copy; 2015 DICCU. All rights reserved. </p>
 		<p>Feel free to download talks, and share them with others, just don't take money for them.</p>
 
 		<h3>This website</h3>
